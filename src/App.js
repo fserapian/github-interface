dca0e1079a520b5ab19e4c2ccd007194f2afb853
Fadi Serapian <fadi.serapian@gmail.com>
2020-08-07 20:11:49 -0400

    put Search and Users components in Home component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
-import Search from './components/users/Search';
 import About from './components/pages/About';
 import User from './components/users/User';
 import Alert from './components/layout/Alert';
+import Home from './components/pages/Home';
 
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
@@ -21,16 +20,7 @@ const App = () => {
             <div className="container">
               <Alert alert={alert} />
               <Switch>
-                <Route
-                  exact
-                  path="/"
-                  render={(props) => (
-                    <Fragment>
-                      <Search />
-                      <Users />
-                    </Fragment>
-                  )}
-                />
+                <Route exact path="/" component={Home} />
                 <Route exact path="/about" component={About} />
                 <Route
                   exact
